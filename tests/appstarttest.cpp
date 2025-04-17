ce77f234b0cedb9aecc253f0e3047ba7fff34fbc
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-04-18 07:25:55 +0900

    appstarttest.cpp を更新

--- tests/appstarttest.cpp
+++ tests/appstarttest.cpp
@@ -37,6 +37,20 @@ void AppStartTest::testMainWindowStartup()
         screenshot.save("screenshot.png"); // スクリーンショットを保存
     }
 
+#if defined(Q_OS_MAC)
+    // macOSではウィンドウボタンが左上なので、左上の赤い「閉じる」ボタンをクリック
+    QPoint closeButton = windowRect.topLeft() + QPoint(10, 10);  // 適宜微調整
+#else
+    // WindowsやLinuxでは右上
+    QPoint closeButton = topRight - QPoint(20, -10);  // ウィンドウの外枠右上から少し左下へオフセット
+#endif
+
+    QTest::mouseClick(QApplication::primaryScreen(), Qt::LeftButton, Qt::NoModifier, closeButton);
+
+    // ウィンドウが閉じるのを確認
+    QTRY_VERIFY_WITH_TIMEOUT(!mainWindow.isVisible(), 3000);
+
+
     // 結果の検証 (必要なら追加)
 //    QCOMPARE(mainWindow.customizeSaveFolder(), mockFolder); //　customizeSaveFolder() は MainWindow 側のメソッド
 }
