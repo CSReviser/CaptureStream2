3478430bafce051f0091ee4d35938f19dda2f22d
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-04-17 22:40:44 +0900

    appstarttest.cpp を更新

--- tests/appstarttest.cpp
+++ tests/appstarttest.cpp
@@ -1,1 +1,22 @@
+#include <QtTest/QtTest>
+#include "../mainwindow.h"  // アプリケーションのヘッダーをインクルード
 
+class AppStartTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testMainWindowStartup();
+};
+
+void AppStartTest::testMainWindowStartup()
+{
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    // ウィンドウがアクティブになるまで待機
+    QVERIFY(QTest::qWaitForWindowActive(&mainWindow));
+}
+
+QTEST_MAIN(AppStartTest)
+#include "appstarttest.moc"
