76db8785efbbe4f26fc75f68ab7d01b07327cd5a
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-06-23 23:56:41 +0900

    settingsmanager.h を更新

--- core/settingsmanager.h
+++ core/settingsmanager.h
@@ -138,5 +138,56 @@ private:
 #endif // SETTINGSMANAGER_H
 
 
+#ifndef SETTINGSMANAGER_H
+#define SETTINGSMANAGER_H
 
+#include <QObject>
+#include <QMap>
+#include <QString>
+
+namespace AppSettings {
+    extern const QList<QString> kCheckBoxKeys;
+    extern const QList<bool> kCheckBoxDefaults;
+    extern const QList<QString> kTextComboBoxKeys;
+    extern const QList<QString> kTextComboBoxDefaults;
+    extern const QList<QString> kOptionalIdKeys;
+    extern const QList<QString> kOptionalDefaultIds;
+    extern const QList<QString> kOptionalTitleKeys;
+    extern const QList<QString> kOptionalDefaultTitles;
+    extern const QList<QString> kSpecialIdKeys;
+    extern const QList<QString> kSpecialDefaultIds;
+    extern const QList<QString> kSpecialTitleKeys;
+    extern const QList<QString> kSpecialDefaultTitles;
+}
+
+class SettingsManager : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsManager(QObject* parent = nullptr);
+
+    void loadSettings();
+    void saveSettings();
+    void resetToDefaults();
+
+    const QMap<QString, bool>& getCheckBoxStates() const;
+    const QMap<QString, QString>& getTextComboBoxValues() const;
+    const QMap<QString, QString>& getOptionalIdMap() const;
+    const QMap<QString, QString>& getOptionalTitleMap() const;
+    const QMap<QString, QString>& getSpecialIdMap() const;
+    const QMap<QString, QString>& getSpecialTitleMap() const;
+
+    static QString applicationBundlePath();
+
+private:
+    QMap<QString, bool> checkBoxStates;
+    QMap<QString, QString> textComboBoxValues;
+    QMap<QString, QString> optionalIdMap;
+    QMap<QString, QString> optionalTitleMap;
+    QMap<QString, QString> specialIdMap;
+    QMap<QString, QString> specialTitleMap;
+};
+
+#endif // SETTINGSMANAGER_H
 
