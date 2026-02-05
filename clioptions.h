c15c2973edd3fdaad0217f2e11f9a1ee71014e19
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-02-05 21:53:02 +0900

    clioptions.h を作成

---
+++ clioptions.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <QString>
+#include <vector>
+#include <optional>
+
+struct CliOptions {
+    bool nogui = false;
+
+    std::vector<QString> programIds;
+
+    std::optional<QString> titleTagFormat;   // -t
+    std::optional<QString> fileNameFormat;   // -f
+    std::optional<QString> outputFolder;     // -o
+    std::optional<QString> extension;        // -e
+
+    bool optionZ = false;  // -z
+    bool optionB = false;  // -b
+    bool optionS = false;  // -s
+
+    bool hasProgramIds() const {
+        return !programIds.empty();
+    }
+};
