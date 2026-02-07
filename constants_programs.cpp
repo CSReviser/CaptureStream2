3591f0c73092e849d4facec4e65c8b740cd92bc5
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-02-07 14:58:59 +0900

    constants_programs.cpp を作成

---
+++ constants_programs.cpp
@@ -0,0 +1,26 @@
+#include "constants_programs.h"
+
+namespace Constants {
+
+const ProgramDefinition EnglishPrograms[] = {
+   ...
+};
+
+const int EnglishCount =
+    sizeof(EnglishPrograms) / sizeof(EnglishPrograms[0]);
+
+const ProgramDefinition OptionalPrograms[] = {
+   ...
+};
+
+const int OptionalCount =
+    sizeof(OptionalPrograms) / sizeof(OptionalPrograms[0]);
+
+const ProgramDefinition SpecPrograms[] = {
+   ...
+};
+
+const int SpecCount =
+    sizeof(SpecPrograms) / sizeof(SpecPrograms[0]);
+
+}
