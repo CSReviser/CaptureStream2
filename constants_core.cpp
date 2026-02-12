ec311e2f539cc53ca69940bf8518cfa10fe74d7d
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-02-12 22:48:48 +0900

    constants_core.cpp を作成

---
+++ constants_core.cpp
@@ -0,0 +1,45 @@
+#include "constants_core.h"
+
+namespace Constants {
+
+    const char* const AppName    = "語学講座ＣＳ２";
+    const char* const AppVersion = "2026/02/10";
+
+    const char* const IniFileName = "CaptureStream2.ini";
+    
+    const char* const SETTING_GROUP_MainWindow      = "MainWindow";
+    const char* const SETTING_GROUP_CustomizeDialog = "CustomizeDialog";
+    const char* const SETTING_GROUP_MessageWindow   = "MessageWindow";
+    const char* const SETTING_GROUP_ScrambleDialog  = "ScrambleDialog";
+    const char* const SETTING_GROUP_Settingsdialog  = "Settingsdialog";
+    
+    const char* const KEY_AudioExtension     = "audio_extension";
+    const char* const DEFAULT_AudioExtension = "m4a";
+
+    const char* const KEY_SaveFolder     = "save_folder";
+    const char* const KEY_FfmpegFolder   = "ffmpeg_folder";
+    
+    const char* const KEY_basic0     = "basic0";
+    const char* const KEY_basic1     = "basic1";
+    const char* const KEY_basic2     = "basic2";
+    const char* const KEY_timetrial  = "timetrial";
+    const char* const KEY_enjoy      = "enjoy";
+    const char* const KEY_kaiwa      = "kaiwa";
+    const char* const KEY_business   = "business1";
+    const char* const KEY_gendai     = "gendai";
+ 
+    const char* const KEY_KOZA_SEPARATION = "koza_separation";
+    const char* const KEY_MULTI_GUI       = "multi_gui";
+    const char* const KEY_NAME_SPACE      = "name_space";
+    const char* const KEY_TAG_SPACE       = "tag_space"; 
+    const char* const KEY_NOGUI           = "nogui";
+    const char* const KEY_LAST_WEEK       = "last_week";
+    const char* const KEY_BOTH_WEEKS      = "both_weeks";
+    const char* const KEY_PROGRAM_LIST    = "program_list";
+    
+    const char* const KEY_CUSTOMIZED_TITLE1    = "customized_title1";
+    const char* const KEY_CUSTOMIZED_TITLE2    = "customized_title2";
+    const char* const KEY_CUSTOMIZED_FILENAME1 = "customized_file_name1";
+    const char* const KEY_CUSTOMIZED_FILENAME2 = "customized_file_name2";
+    
+}
