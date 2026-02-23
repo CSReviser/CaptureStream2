6ca2f828ae1d693072d8638003db83d8255c72fe
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-02-23 15:39:30 +0900

    programresolver.h を作成

---
+++ programresolver.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <QString>
+#include <QStringList>
+
+struct ResolveResult
+{
+    enum Status {
+        NotFound,
+        Unique,
+        Ambiguous
+    };
+
+    Status status = NotFound;
+    QString id;            // Unique のときのみ有効
+    QStringList candidates; // Ambiguous のときのみ
+};
+
+class ProgramResolver
+{
+public:
+    static ResolveResult resolve(const QString& input);
+
+private:
+    static QString normalize(const QString& s);
+};
