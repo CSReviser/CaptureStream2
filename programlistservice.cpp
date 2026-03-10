f939724196823f2ad35d7355be9479892a1098c7
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-03-11 00:04:20 +0900

    programlistservice.cpp を作成

---
+++ programlistservice.cpp
@@ -0,0 +1,34 @@
+#include "programlistservice.h"
+#include "programrepository.h"
+#include "constants_program.h"
+
+QStringList ProgramListService::buildProgramList(int type)
+{
+    auto &repo = ProgramRepository::instance();
+
+    QStringList allKeys = repo.name_map.keys();
+    QStringList result;
+
+    switch (type) {
+
+    case English:
+        for (const QString& name : allKeys) {
+            if (Constants::matchEnglish(name))
+                result << name;
+        }
+        break;
+
+    case OtherLanguages:
+        for (const QString& name : allKeys) {
+            if (Constants::matchOtherLanguages(name))
+                result << name;
+        }
+        break;
+
+    case All:
+        result = allKeys;
+        break;
+    }
+
+    return result;
+}
