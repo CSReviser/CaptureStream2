892070ad5d7d4a16ef05e80cc0655a189966396f
CSReviser <CSReviser@gmail.com>
2026-03-13 16:59:10 +0900

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -165,12 +165,12 @@ MainWindow::MainWindow( Settings& settings, QWidget *parent )
 ///	setMaximumHeight( maximumHeight() - menuBar()->height() );
 //	setMinimumHeight( maximumHeight() - menuBar()->height() );
 	menuBar()->setNativeMenuBar(false);
-	setMaximumHeight( maximumHeight() + ( menuBar()->height() - 24 ) * 2 );	// レコーディングボタンが表示されない問題対策　2024/06/06
-	setMinimumHeight( maximumHeight() + ( menuBar()->height() - 24 ) * 2 );	// レコーディングボタンが表示されない問題対策　2024/06/06
+	setMaximumHeight( maximumHeight() + ( menuBar()->height() - 10 ) * 2 );	// レコーディングボタンが表示されない問題対策　2024/06/06
+	setMinimumHeight( maximumHeight() + ( menuBar()->height() - 10 ) * 2 );	// レコーディングボタンが表示されない問題対策　2024/06/06
 	if (settings.checked[QString::fromUtf8(Constants::KEY_MAC_MENUBAR)]){
 		menuBar()->setNativeMenuBar(true);
-		setMaximumHeight( maximumHeight() - ( menuBar()->height() - 24 ) * 2 );
-		setMinimumHeight( maximumHeight() - ( menuBar()->height() - 24 ) * 2 );
+		setMaximumHeight( maximumHeight() - ( menuBar()->height() - 0 ) * 2 );
+		setMinimumHeight( maximumHeight() - ( menuBar()->height() - 0 ) * 2 );
 	} 
 //	setMaximumHeight( maximumHeight() );		// ダウンロードボタンが表示されない問題対策　2022/04/16
 //	setMinimumHeight( maximumHeight() );		// ダウンロードボタンが表示されない問題対策　2022/04/16
