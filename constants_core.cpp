999d71ca2c72e567052fcb12ab5aa040e16be253
CSReviser <CSReviser@gmail.com>
2026-03-21 11:09:59 +0900

    update

--- constants_core.cpp
+++ constants_core.cpp
@@ -27,7 +27,7 @@ namespace Constants {
 
     // ===== アプリ情報 =====
     const char* const AppName    = "語学講座ＣＳ２";
-    const char* const AppVersion = "2026/03/20";
+    const char* const AppVersion = "2026/03/21";
 
     // ===== INI ファイル名 =====
     const char* const IniFileName = "CaptureStream2.ini";
