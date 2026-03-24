8cdc85315ab751634beca115be67bae0459adc7c
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-03-25 05:31:37 +0900

    episodeinfo.h を更新

--- episodeinfo.h
+++ episodeinfo.h
@@ -9,9 +9,29 @@ struct EpisodeInfo {
     QString rawTitle;
     QString kouza;
 
+    QDate date;
+    QTime time;
+
+    QString nendo;
+    QString fileId;
+    QString thumbnailPath;
+
+    QString dupnmb;
+
+    bool isXml = false;  // 🔥 追加
+};
+
+/*
+struct EpisodeInfo {
+    QString inputUrl;
+
+    QString rawTitle;
+    QString kouza;
+
     QDate   date;      // 🔥 統一
     QString nendo;     // 表示用（Formatter用）
 
     QString fileId;    // 元ファイル名ベース
     QString thumbnailPath;
 };
+*/
