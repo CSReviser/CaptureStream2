4f3dd6ca7cd2bc525667174287cfceabb041660d
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-03-24 10:12:57 +0900

    thumbnailoptionsbuilder.cpp を更新

--- thumbnailoptionsbuilder.cpp
+++ thumbnailoptionsbuilder.cpp
@@ -76,3 +76,30 @@ QStringList ThumbnailOptionsBuilder::buildOutput(const RecordingRequest& req)
 
     return args;
 }
+
+QStringList ThumbnailOptionsBuilder::buildOutput(const RecordingRequest& req)
+{
+    QStringList args;
+
+    if (!req.thumbnail.enabled)
+        return args;
+
+    if (req.thumbnail.imagePath.isEmpty())
+        return args;
+
+    if (req.container == Container::MP3 ||
+        req.container == Container::M4A) {
+
+        args << "-map" << "0:a";
+        args << "-map" << "1:v";
+
+        args << "-map_metadata" << "0";
+
+        // 音声はBuilder側に任せる
+
+        args << "-c:v" << "mjpeg";
+        args << "-disposition:v" << "attached_pic";
+    }
+
+    return args;
+}
