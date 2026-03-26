193e298b27ebbaf6d449119033b5533a020b61da
CSReviser <46049273+CSReviser@users.noreply.github.com>
2026-03-26 11:41:47 +0900

    ffmpegrunner.h を更新

--- ffmpegrunner.h
+++ ffmpegrunner.h
@@ -35,18 +35,22 @@ public:
 public:
     FfmpegRunner();
 
-    Result run(const FfmpegRunRequest& req);
+    // ★ 修正
+    Result run(const FfmpegRunRequest& plan);
 
     void requestCancel();
     void setLogCallback(LogCallback cb);
 
 private:
-    int runOnce(const FfmpegRunRequest& req, QString& outLog);
+    // ★ 修正
+    int runOnce(const FfmpegRunRequest& plan, QString& outLog);
 
     bool shouldRetry(int exitCode, const QString& log) const;
     bool isPermanentError(const QString& log) const;
 
-    QString makeTempPath(const FfmpegRunRequest& req) const;
+    // ★ 修正
+    QString makeTempPath(const FfmpegRunRequest& plan) const;
+
     bool safeReplace(const QString& tempPath, const QString& finalPath) const;
 
 private:
